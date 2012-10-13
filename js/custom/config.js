9dc511f29e11b22edf453c523e8cc69223232237
cypi <epicyr@gmail.com>
2012-10-13 13:07:57 +0200

    corriger les conneries de jerem

---
+++ js/custom/config.js
@@ -0,0 +1,20 @@
+
+/** global configuration variable **/
+
+var config = {
+
+	//host configuration
+	"hostname":"192.168.0.11",
+	"port":"8080",
+	"rootPath":"scrumble-server-web/scrumble",
+
+	//here are listed all available resources for Rest Webservices. Don't forget to update it each time a new resource is created
+	"resources": {
+			"members":"members",
+			"projects":"projects",
+			"userStories":"userstories",
+			"tasks":"tasks",
+		}
+
+
+};
