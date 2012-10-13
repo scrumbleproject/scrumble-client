73f4973ad862f919f25ece851de23978106142a7
cypi <epicyr@gmail.com>
2012-10-13 16:05:50 +0200

    clean file structure

--- js/json.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $.getJSON('http://localhost/users.json',function(data){
-        $.each(data, function (entryIndex, entry){
-            html+=entry;
-        });
-        alert("JSON Data: " + json.nom);
-    });
-    
-});
