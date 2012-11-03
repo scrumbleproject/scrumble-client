b97a39fe5f1812f3f73ea222292d2e8952e62cde
Keiit <arnaud.delacruz@gmail.com>
2012-11-03 15:20:38 +0100

    Pb pull

--- js/custom/task.js
+++ js/custom/task.js
@@ -1,7 +1,7 @@
 
 /** Tasks methods **/
 
-//fill the form with data about one member
+//fill the form with data about one task
 function fillForm(response) {
   $("#idMember").val(response.idMember);
   //$("#idRole").val(response.idRole);
@@ -11,3 +11,128 @@ function fillForm(response) {
   $("#email").val(response.email);
   $("#password").val(response.password);
 }
+
+//display all items
+function displayAllItems(items){
+	if (items.member1.length>1){ //if more than one tasks
+		$("#memberList > tbody").html("");
+		$.each(items.member1, function(i, dico){
+			$("#memberList > tbody").append("<tr>");
+			$("#memberList > tbody").append("<td>"+(i+1)+"</td>");
+			$("#memberList > tbody").append("<td>"+dico.firstname+"</td>");
+			$("#memberList > tbody").append("<td>"+dico.lastname+"</td>");
+			$("#memberList > tbody").append("<td>"+dico.login+"</td>");
+			$("#memberList > tbody").append("<td></td>");
+			$("#memberList > tbody").append("<td>"+dico.email+"</td>");
+			$("#memberList > tbody").append("<td><a class='btn' href='member.html?idMember="+dico.idMember+"'><i class='icon-pencil'></i></a>" +
+          "<a class='btn btn-danger btn-danger btn-delete' href='"+dico.idMember+"'><i class='icon-trash'></i></a></td>");
+			$("#usersList > tbody").append("</tr>");
+		});   
+	}
+	else { //if only one member
+		$("#memberList > tbody").append("<tr>");
+		$("#memberList > tbody").append("<td>"+(i+1)+"</td>");
+		$("#memberList > tbody").append("<td>"+items.member1.firstname+"</td>");
+		$("#memberList > tbody").append("<td>"+items.member1.lastname+"</td>");
+		$("#memberList > tbody").append("<td>"+items.member1.login+"</td>");
+		$("#memberList > tbody").append("<td></td>");
+		$("#memberList > tbody").append("<td>"+items.member1.email+"</td>");
+		$("#memberList > tbody").append("<td><a class='btn' href='member.html?idMember="+items.member1.idMember+"'><i class='icon-pencil'></i></a>" +
+          "<a class='btn btn-danger btn-danger btn-delete' href='"+items.member1.idMember+"'><i class='icon-trash'></i></a></td>");
+		$("#memberList > tbody").append("</tr>");
+	}
+}
+
+/** Put here all calls that you want to launch at the page startup **/		
+$(document).ready( function() {
+	
+	//load left-menu	
+	/*$('#left-menu').load('leftMenu.html', function(response, status, xhr) {
+		if (status == "error") {
+			var msg = "Sorry leaf-menu cannot be loaded: ";
+			bootbox.alert(msg + xhr.status + " " + xhr.statusText);
+		}
+		else { //if successful
+			//select the related option in left-menu
+			$("li#left-menu-option-member").addClass("active");
+		} 		
+	});*/
+
+	//get param idMember in url if exists
+    /*var idMember = $(document).getUrlParam("idMember");		
+	
+	//load data on list or on form
+    if ( (idMember !=="") && (idMember !==null)) {
+        $.ajax({
+            url:'http://'+config.hostname+':'+config.port+'/'+config.rootPath+'/'+config.resources.tasks+'/'+idMember,
+            type:'GET',
+            contentType:'application/json; charset=UTF-8',
+            success: function(reponse) {
+                fillForm($.parseJSON(reponse));
+            },
+	        error:function (xhr, status, error){
+		        bootbox.alert('Erreur : '+xhr.responseText+' ('+status+' - '+error+')');
+	        },
+            dataType: 'text',
+            converters: 'text json'
+        });
+	                      
+    }
+	else {
+	    $.ajax({
+            url:'http://'+config.hostname+':'+config.port+'/'+config.rootPath+'/'+config.resources.tasks+'/all',
+            type:'GET',
+		    contentType:'application/json; charset=UTF-8',
+            success: function(reponse) {
+                displayAllItems($.parseJSON(reponse));
+				bindDeleteEvent();
+            },
+		    error:function (xhr, status, error){
+			    bootbox.alert('Erreur : '+xhr.responseText+' ('+status+' - '+error+')');
+		    },
+		    dataType: 'text',
+		    converters: 'text json'
+	    });	
+    }*/
+
+	//action on #formUser form
+	/*$('#formTask').submit(function() {
+		
+		//Get #idMember field value	
+		var idMember = $("#idMember").val();
+
+		if (idMember==null || idMember.length==0) {
+			//Case 1 : create a new member (idMember is empty)
+		    $.ajax({
+		        url:'http://'+config.hostname+':'+config.port+'/'+config.rootPath+'/'+config.resources.members+'/add',
+		        type:"POST",
+		        data: JSON.stringify($('#formUser').serializeObject()),
+		        dataType: "json",
+		        contentType: "application/json; charset=utf-8",
+		        success: function(data) {
+		                bootbox.alert('Member has been added successfully.');
+						window.location.replace('memberList.html'); //redirect to memberList.html
+		        },
+				error:function (xhr, status, error){
+					bootbox.alert('Erreur : '+xhr.responseText+' ('+status+' - '+error+')');
+				}
+		    });
+		}
+		else { //Case 2 : update an existing member (idMember is not empty)
+			$.ajax({
+                url:'http://'+config.hostname+':'+config.port+'/'+config.rootPath+'/'+config.resources.members,
+                type:"PUT",
+                data: JSON.stringify($('#formUser').serializeObject()),
+                dataType: "json",
+                contentType: "application/json; charset=utf-8",
+                success: function(data) {
+                    bootbox.alert("Member has been updated successfully.");
+					window.location.replace('memberList.html'); //redirect to memberList.html
+                }
+            });
+		}
+
+	    return false;
+    });*/
+    
+});
