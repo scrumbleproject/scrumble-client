a7b784778aa322076d8527875c29aa91679d7d53
cypi <epicyr@gmail.com>
2012-11-11 16:23:24 +0100

    config.js

--- js/custom/config.js
+++ js/custom/config.js
@@ -4,8 +4,8 @@
 var config = {
 
 	//host configuration
-	"hostname":"localhost",
-	"port":"8080",
+	"hostname":"89.2.88.18",//"192.168.0.16",
+	"port":"8082",//"8080",
 	"rootPath":"scrumble-server-web/scrumble",
 
 	//here are listed all available resources for Rest Webservices. Don't forget to update it each time a new resource is created
